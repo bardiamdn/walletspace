c214574911921513a7f26f3571b9b206e4e04e99
bardia <bardia7madani8@gmail.com>
2024-07-22 13:48:52 +0300

    Integrate Google DocumentAI in API
    - Remove CI file to be implemented in the future
    - Set up DocumentProcessorServiceClient for document processing

--- apps/api/src/routes/scanner.route.ts
+++ apps/api/src/routes/scanner.route.ts
@@ -1,10 +1,15 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import multer from 'multer';
+import { promises as fs } from 'fs';
 import path from 'path';
+import dotenv from 'dotenv';
+import { DocumentProcessorServiceClient } from '@google-cloud/documentai'
 
 import authMiddleware from '../middlewares/authMiddleware';
 import { Scan } from '../db/entities/Scan';
 import { AppDataSource } from '../db/dataSource';
+
+dotenv.config()
 
 const router = Router();
 
@@ -46,9 +51,35 @@ router.post('/scanner', authMiddleware, upload.single('image'), async (req: Requ
     scan.file_name = file.filename;
     scan.path = file.path;
     scan.size = file.size;
+    
+    const client = new DocumentProcessorServiceClient({apiEndpoint: 'eu-documentai.googleapis.com'});
+
+    // const documentAIUrl = `https://${process.env.GOOGLE_DOCUMENT_AI_LOCATION}-documentai.googleapis.com/v1/projects/${process.env.GOOGLE_DOCUMENT_AI_PROJECT_ID}/locations/${process.env.GOOGLE_DOCUMENT_AI_LOCATION}:fetchProcessorTypes`;
+
+    const name = `projects/${process.env.GOOGLE_DOCUMENT_AI_PROJECT_ID}/locations/${process.env.GOOGLE_DOCUMENT_AI_LOCATION}/processors/${process.env.GOOGLE_DOCUMENT_AI_OCR_SCANNER_PROCESSOR_ID}`;
+    const imagePath = path.resolve('./' + file.path);
+    const imageFile = await fs.readFile(imagePath);
+    console.log(imagePath)
+    console.log(name)
+
+    // Convert the image data to a Buffer and base64 encode it.
+    const encodedImage = Buffer.from(imageFile).toString('base64');
+    const fieldMask = 'result.document.text'
+    
+    const documentAIRequest = {
+      name,
+      rawDocument: {
+        content: encodedImage,
+        mimeType: file.mimetype,
+      },
+      fieldMask: { paths: ['text'] }
+    };
+
+    const [result] = await client.processDocument(documentAIRequest);
+    const { document } = result;
   
     await AppDataSource.manager.save(scan);
-    return res.status(200).json({ file: file, authInfo: req.jwt });
+    return res.status(200).json({ file: file, authInfo: req.jwt, document: document });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Server error' });
