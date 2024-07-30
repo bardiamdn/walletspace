878fa80bdb78a6cbfaecd118d4c4711f82882e53
bardia <bardia7madani8@gmail.com>
2024-07-30 14:03:53 +0300

    chore(webapp): initialize Next.js and Tailwind CSS

---
+++ apps/webapp/app/layout.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Welcome',
+  description: 'Description',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
